f60b6e7e87e4a478e0974e8184924677c706b358
Surya Oktafendri <f2face@f2face.com>
2024-11-04 03:27:06 +0700

    Cleanup

--- src/R2.ts
+++ src/R2.ts
@@ -1,17 +1,23 @@
-import { CreateBucketCommand, DeleteBucketCommand, ListBucketsCommand, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
+import {
+    CreateBucketCommand,
+    DeleteBucketCommand,
+    ListBucketsCommand,
+    S3Client,
+    type S3ClientConfig,
+} from '@aws-sdk/client-s3';
 import { Bucket } from './Bucket';
 import type { BucketList, CORSPolicy, CloudflareR2Config } from './types';
 
 export class R2 {
-    private config: CloudflareR2Config;
-    private r2: S3Client;
-    public endpoint: string;
+    private readonly config: Readonly<CloudflareR2Config>;
+    private readonly r2: S3Client;
+    public readonly endpoint: S3ClientConfig['endpoint'];
 
     constructor(config: CloudflareR2Config, overrides?: S3ClientConfig) {
         this.config = config;
 
         if (overrides?.endpoint) {
-            this.endpoint = overrides.endpoint
+            this.endpoint = overrides.endpoint;
         } else if (this.config.jurisdiction) {
             this.endpoint = `https://${this.config.accountId}.${this.config.jurisdiction}.r2.cloudflarestorage.com`;
         } else {
@@ -25,7 +31,7 @@ export class R2 {
                 secretAccessKey: this.config.secretAccessKey,
             },
             region: 'auto',
-            ...overrides
+            ...overrides,
         });
     }
 
